d54ac6a8582d7dc11af93e21926b80fe5be72a15
khanh.1609 <khanhkelly16@gmail.com>
2021-07-23 17:30:43 +0700

    ,,,,

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,36 +24,47 @@ for(var i=0; i < menuItems.length;i++){
 
 
 //Slide 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName(".caroule-item");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("carousel-item");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
 }
-
-var slideIndex = 1;
- showSlides(slideIndex);
-function showSlides(n) {
-   var i;
-   var slides = document.getElementsByClassName("carousel-item");
-   var dots = document.getElementsByClassName("dot");
-   if (n > slides.length) {slideIndex = 1}
-   if (n < 1) {slideIndex = slides.length}
-   for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-   }
-   for (i = 0; i < dots.length; i++) {
-       dots[i].className = dots[i].className.replace("active", "");
-   }
-    //chuyển đến slide tiếp theo
-    slideIndex++;
-    //nếu đang ở slide cuối cùng thì chuyển về slide đầu
-    if (slideIndex > slides.length -1) {
-      slideIndex = 0;
-    }    
-    //tự động chuyển đổi slide sau 5s
-    setTimeout(showSlides, 7000);
-
-  slides[slideIndex].style.display = "block"
-  dots[slideIndex].getElementsByClassName += "active";
- }
- showSlides(slideIndex = 1);
 
     
